72283b104fb9e6ac23d7fe1c9db924abe261210a
Forrest Filler <forrestfiller@users.noreply.github.com>
2018-05-21 13:56:04 -0400

    templating for meeting

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router()
 
-router.get('/', function(req, res, next){
+router.get('/goals', function(req, res, next){
 
-	res.json({
-		confirmation:'success',
-		message: 'success'
-	});
+	res.render('goals', { title: 'Fund Goals' })
+
+	// res.json({
+	// 	confirmation:'success',
+	// 	message: 'success'
+	// });
 
 });
 
